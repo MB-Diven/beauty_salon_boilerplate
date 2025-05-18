581da9b99ffb0fb4fe7f42109f53f2adfee45f15
NoCluePS <pijus.serap@gmail.com>
2025-05-18 18:54:48 +0300

    bim bam bum bim

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // App Router specific settings
-  swcMinify: true, // Better minification
-  output: "standalone", // Optimized for Netlify deployment
+  output: "export", // Optimized for Netlify deployment
 };
 
 export default nextConfig;
